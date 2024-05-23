5957ccb70aedf0df4f568acf07d4459c32f18a1d
kiragit01 <kiragithub@gmail.com>
2024-05-23 21:58:31 +0300

    Добавьте файлы проекта.

---
+++ Book.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP2
+{
+    class Book
+    {
+        public Title title;
+        public Author author;
+        public Content content;
+
+        public Book(Title title, Author author, Content content)
+        {
+            this.title = title;
+            this.author = author;
+            this.content = content;
+        }
+
+        public void Show()
+        {
+            title.Show();
+            author.Show();
+            content.Show();
+        }
+    }
+    class Title
+    {
+        private string title;
+        public Title(string title)
+        {
+            this.title = title;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Title: {title}");
+            Console.ResetColor();
+        }
+    }
+    class Author
+    {
+        private string author;
+        public Author(string author)
+        {
+            this.author = author;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Author: {author}");
+            Console.ResetColor();
+        }
+    }
+    class Content
+    {
+        private string content;
+        public Content(string content)
+        {
+            this.content = content;
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Content: {content}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Title: ");
+            string _title = Console.ReadLine();
+            Title title = new Title(_title);
+
+            Console.Write("Author: ");
+            string _author = Console.ReadLine();
+            Author author = new Author(_author);
+
+            Console.Write("Content: ");
+            string _content = Console.ReadLine();
+            Content content = new Content(_content);
+
+            Book book = new Book(title, author, content);
+
+            book.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
